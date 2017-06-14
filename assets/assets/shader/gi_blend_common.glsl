79cad01d91e47e4b134815aebcc435087ac89082
lowkey <mephistopheles@onlinehome.de>
2017-06-14 16:12:04 +0200

    implemented joint bilateral upsampling

--- assets/assets/shader/gi_blend_common.glsl
+++ assets/assets/shader/gi_blend_common.glsl
@@ -37,11 +37,9 @@ vec3 calculate_gi(vec2 uv, vec2 gi_uv, int gi_lod, sampler2D diff_sampler, sampl
 
 
 	// load diff + spec GI
-	vec3 radiance = upsampled_result(diff_sampler, gi_lod,
-	                                 gi_uv, max(1.0, pow(2.0, gi_lod-1.0))).rgb;
+	vec3 radiance = upsampled_result(diff_sampler, 0, gi_lod, gi_uv).rgb;
 
-	vec3 specular = upsampled_smooth(spec_sampler, 0.0,
-	                                 gi_uv, 2.0).rgb;
+	vec3 specular = upsampled_result(spec_sampler, 0, 0, gi_uv).rgb;
 
 	// no raycast hit => fallback
 	specular.rgb = max(radiance * 0.05, specular.rgb);
--- assets/assets/shader/upsample.glsl
+++ assets/assets/shader/upsample.glsl
@@ -5,102 +5,45 @@
 #include "random.glsl"
 
 
-vec4 upsampled_smooth(sampler2D color_sampler, float lod, vec2 tex_coords, float scale) {
-	if(lod > pcs.arguments.y)
-		return vec4(0,0,0,0);
-
-	vec2 texture_size = textureSize(color_sampler, int(lod+0.5));
-	vec2 rand = PDnrand2(vec4(tex_coords, lod, 0.0));
-
-	vec4 c = vec4(0,0,0,0);
-	float weight = 0.0;
-	for(int i=0; i<8; i++) {
-		vec2 offset = vec2(Poisson8[i].x*rand.x - Poisson8[i].y*rand.y, Poisson8[i].x*rand.y + Poisson8[i].y*rand.x);
-
-		vec2 uv = tex_coords + offset/texture_size * scale;
-
-		float w = 1.0 - clamp(dot(offset,offset), 0.0, 0.9);
-		c += textureLod(color_sampler, uv, lod) * w;
-		weight += w;
-	}
-
-	return c / weight;
+float weight_offset(float x) {
+	float b = 0;
+	float c = 2.f;
+	return 0.5 * exp(- (x-b)*(x-b) / (2*c*c));
+}
+float weight_depth(float x) {
+	float b = 0;
+	float c = 0.01f;
+	return 0.1 * exp(- (x-b)*(x-b) / (2*c*c));
 }
 
-vec4 upsampled_max(sampler2D color_sampler, float lod, vec2 tex_coords, float scale) {
-	if(lod > pcs.arguments.y)
+vec4 upsampled_result(sampler2D color_sampler, int depth_lod, int color_lod, vec2 tex_coords) {
+	if(color_lod > pcs.arguments.y)
 		return vec4(0,0,0,0);
-
-	vec2 texture_size = textureSize(color_sampler, int(lod+0.5));
-
-	vec4 c = vec4(0,0,0,0);
-	for(int i=0; i<8; i++) {
-		vec2 uv = tex_coords + Poisson8[i]/texture_size * scale;
-
-		c = max(c, textureLod(color_sampler, uv, lod));
-	}
-
-	return c;
-}
-
-vec4 upsampled_result(sampler2D color_sampler, int lod, vec2 tex_coords, float scale) {
-	if(lod > pcs.arguments.y)
-		return vec4(0,0,0,0);
-
-	vec2 texture_size = textureSize(color_sampler, lod);
-	ivec2 center = ivec2(texture_size*tex_coords);
-
-	float depth = texelFetch(depth_sampler, lod>0 ? center*2 : center, max(0, lod-1)).r;
-
-
-	const vec2 offsets[4] = vec2[](
-		vec2(-0.5, -0.5),
-		vec2( 0.5, -0.5),
-		vec2( 0.5,  0.5),
-		vec2(-0.5,  0.5)
-	);
-
-
-	float depths[4];
-
-	for(int i=0; i<4; i++) {
-		ivec2 uv = ivec2(center + offsets[i]);
-		depths[i] = texelFetch(depth_sampler, uv, lod).r;
-	}
-
-
-	if(abs(depths[0] - depth) > 0.05 ||
-	   abs(depths[1] - depth) > 0.05 ||
-	   abs(depths[2] - depth) > 0.05 ||
-	   abs(depths[3] - depth) > 0.05) {
-		// edge detected
-		vec4 c = vec4(0,0,0,0);
-		float weight_sum = 0.0;
-
-		for(int i=0; i<4; i++) {
-			ivec2 uv = ivec2(center + offsets[i]);
-			float weight = 1.0 / clamp(abs(depth-depths[i]), 0.1, 4.0);
-			c += weight * texelFetch(color_sampler, uv, lod);
+	
+	vec2 texture_size = textureSize(color_sampler, color_lod);
+	float depth = texelFetch(depth_sampler, ivec2(texture_size*tex_coords*2), depth_lod).r;
+	
+	float weight_sum = 0;
+	vec4 color = vec4(0,0,0,0);
+	
+	for(float x=-2; x<=2; x++) {
+		for(float y=-2; y<=2; y++) {
+			vec2 offset = vec2(x,y);
+			
+			float d = texelFetch(depth_sampler, ivec2((texture_size*tex_coords + offset)*2), depth_lod).r;
+			
+			float weight = weight_offset(dot(offset,offset)) * weight_depth(depth-d);
+			
+			color += weight * texelFetch(color_sampler, ivec2(texture_size*tex_coords + offset), color_lod);
 			weight_sum += weight;
 		}
-		return c / weight_sum;
-
-	} else {
-		vec4 c = vec4(0,0,0,0);
-		float weight_sum = 0.0;
-
-		for(int i=0; i<4; i++) {
-			vec2 uv = clamp(offsets[i]/texture_size * 1.2 + tex_coords, vec2(0,0), vec2(1,1));
-			float weight = 1.0;
-			c += weight * textureLod(color_sampler, uv, lod);
-			weight_sum += weight;
-		}
-		return c / weight_sum;
 	}
+	
+	return color / weight_sum;
 }
 
 vec4 upsampled_prev_result(sampler2D color_sampler, int current_lod, vec2 tex_coords) {
-	return upsampled_result(color_sampler, current_lod + 1, tex_coords, 4.0);
+	return upsampled_result(color_sampler, current_lod, current_lod + 1, tex_coords);
 }
 
 #endif
