e0096c272482313dc03b67a0992662e0196b90d0
lowkey <mephistopheles@onlinehome.de>
2017-09-30 22:05:27 +0200

    enabled travis-ci integration

---
+++ .travis.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Build script for travis-ci.org builds to handle compiles and static
+# analysis when ANALYZE=true.
+#
+
+if [ $ANALYZE = "true" ]; then
+    if [ "$CC" = "clang" ]; then
+        docker exec build scan-build cmake -G "Unix Makefiles" -H/repo -B/build
+        docker exec build scan-build -enable-checker core.AdjustedReturnValue \
+          -enable-checker core.AttributeNonNull \
+          -enable-checker deadcode.DeadStores \
+          -enable-checker security.insecureAPI.UncheckedReturn \
+          --status-bugs -v \
+          cmake --build /build
+    fi
+else
+  docker exec build cmake -DSF2_BUILD_TESTS=ON -H/repo -B/build
+  docker exec build cmake --build /build
+  docker exec build cmake --build /build --target test
+fi
+
