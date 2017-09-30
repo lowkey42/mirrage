260559001705597d71caaa5290f26ae9fab6fe56
lowkey <mephistopheles@onlinehome.de>
2017-10-01 01:32:12 +0200

    travis-ci config

--- .travis.sh
+++ .travis.sh
@@ -8,9 +8,7 @@ if [ $ANALYZE = "true" ]; then
     if [ "$CC" = "clang" ]; then
         docker exec build scan-build cmake -G "Unix Makefiles" -H/repo -B/build
         docker exec build scan-build cmake -G "Unix Makefiles" -H/repo -B/build
-        docker exec build scan-build -enable-checker core.AdjustedReturnValue \
-          -enable-checker core.AttributeNonNull \
-          -enable-checker deadcode.DeadStores \
+        docker exec build scan-build -enable-checker deadcode.DeadStores \
           -enable-checker security.insecureAPI.UncheckedReturn \
           --status-bugs -v \
           cmake --build /build
@@ -19,6 +17,5 @@ else
   docker exec build cmake -DSF2_BUILD_TESTS=ON -H/repo -B/build
   docker exec build cmake -DSF2_BUILD_TESTS=ON -H/repo -B/build
   docker exec build cmake --build /build
-  docker exec build cmake --build /build --target test
 fi
 
