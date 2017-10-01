48eae7d435a6caa972d1929317d41863970c790b
lowkey <mephistopheles@onlinehome.de>
2017-10-01 14:45:02 +0200

    removed deprecated std::fun_ptr

--- src/mirrage/utils/include/mirrage/utils/string_utils.hpp
+++ src/mirrage/utils/include/mirrage/utils/string_utils.hpp
@@ -54,14 +54,13 @@ namespace mirrage::util {
 
 	// trim from start
 	inline std::string& ltrim(std::string& s) {
-		s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](auto c) { return !std::isspace(c); }));
 		return s;
 	}
 
 	// trim from end
 	inline std::string& rtrim(std::string& s) {
-		s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
-		        s.end());
+		s.erase(std::find_if(s.rbegin(), s.rend(), [](auto c) { return !std::isspace(c); }).base(), s.end());
 		return s;
 	}
 
