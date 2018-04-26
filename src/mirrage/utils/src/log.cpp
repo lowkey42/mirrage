f4ebbde1fee54ed898e48e7f27e2c77a79e021cf
lowkey <mephistopheles@onlinehome.de>
2018-04-26 10:58:10 +0200

    added ignore for old-style-cast warning in backward-cpp

--- src/mirrage/utils/src/log.cpp
+++ src/mirrage/utils/src/log.cpp
@@ -4,9 +4,11 @@
 #ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#pragma clang diagnostic ignored "-Wold-style-cast"
 #else
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
 #endif
 
 #include <backward.hpp>
