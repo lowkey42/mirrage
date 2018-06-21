83baabe287463572fc371bbb4cf43b64bd1c0ff8
lowkey <mephistopheles@onlinehome.de>
2018-06-21 14:19:32 +0200

    fixed travis CI

--- .travis.sh
+++ .travis.sh
@@ -6,8 +6,8 @@
 
 if [ $ANALYZE = "true" ]; then
     if [ "$CC" = "clang" ]; then
-        docker exec build scan-build --use-c++=clang++ cmake -DMIRRAGE_FORCE_LIBCPP=ON -G "Unix Makefiles" -H/repo -B/build
-        docker exec build scan-build --use-c++=clang++ -enable-checker deadcode.DeadStores \
+        docker exec build scan-build --use-c++=clang++ --use-cc=clang cmake -DMIRRAGE_FORCE_LIBCPP=ON -DMIRRAGE_ENABLE_COTIRE=OFF -G "Unix Makefiles" -H/repo -B/build
+        docker exec build scan-build --use-c++=clang++ --use-cc=clang -enable-checker deadcode.DeadStores \
           -enable-checker security.insecureAPI.UncheckedReturn \
           --status-bugs -v \
           cmake --build /build
