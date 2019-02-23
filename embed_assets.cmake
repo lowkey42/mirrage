275b18bb6d837477549e6ddc86e92f1df8eaf058
Lotrado <lotrado@onlinehome.de>
2019-02-23 01:14:27 +0100

    Changed mirrage_embed_asset to keyword-based parameters to support an option EXPORT parameter, specifying the export set that is required to install the object library containing the generated .res file on MSVC.

--- embed_assets.cmake
+++ embed_assets.cmake
@@ -1,8 +1,13 @@
 cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
 
-#optional: generated files to depend on
-macro(mirrage_embed_asset target src_files)
-	string (REPLACE ";" "$<SEMICOLON>" src_files_str "${src_files}")
+macro(mirrage_embed_asset)
+	set(options)
+	set(oneValueArgs TARGET EXPORT)
+	set(multiValueArgs SOURCES DEPENDS)
+	cmake_parse_arguments(EMBED_ASSET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+	set(target ${EMBED_ASSET_TARGET})
+
+	string (REPLACE ";" "$<SEMICOLON>" src_files_str "${EMBED_ASSET_SOURCES}")
 
 	set(ID "mirrage_embedded_asset_${target}")
 	if(MSVC)
@@ -18,7 +23,7 @@ macro(mirrage_embed_asset target src_files)
 	
 	add_custom_command(OUTPUT ${EMBED_SRC_FILE}
 		COMMAND ${CMAKE_COMMAND} -DMIRRAGE_ROOT_DIR=${MIRRAGE_ROOT_DIR} -DEMBED_MODE=${EMBED_MODE} -DID=${ID} -DSRC_FILES=${src_files_str} -DDST_DIR=${CMAKE_CURRENT_BINARY_DIR} -DEMBED_SRC_FILE=${EMBED_SRC_FILE} -P ${MIRRAGE_ROOT_DIR}/embed_recursive_into_asm.cmake
-		DEPENDS ${ARGN}
+		DEPENDS ${EMBED_ASSET_SOURCES} ${EMBED_ASSET_DEPENDS}
 		VERBATIM
 	)
 
@@ -79,6 +84,11 @@ void ref_embedded_assets_${target}() {
 		add_library(mirrage_embedded_assets_obj_${target} OBJECT "${EMBED_SRC_FILE}")
 		target_link_libraries(${target} INTERFACE $<TARGET_OBJECTS:mirrage_embedded_assets_obj_${target}>)
 		add_dependencies(mirrage_embedded_assets_obj_${target} mirrage_embedded_assets_${target})
+		if(EMBED_ASSET_EXPORT)
+			install(TARGETS mirrage_embedded_assets_obj_${target}
+					EXPORT ${EMBED_ASSET_EXPORT}
+					OBJECTS DESTINATION lib)
+		endif()
 	endif()
 endmacro()
 
