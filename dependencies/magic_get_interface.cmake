9393b619a55d68975886d30b776d7e8a32906eca
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2019-03-25 16:41:33 +0100

    Removed outdated workaround.

--- dependencies/magic_get_interface.cmake
+++ dependencies/magic_get_interface.cmake
@@ -8,13 +8,6 @@ target_include_directories(pfr INTERFACE
 	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/magic_get/include>
 	$<INSTALL_INTERFACE:include>)
 
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-	# GCC broke their structured binding implementation in 7.2.0.
-	# See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81888
-	# TODO: Remove when fixed.
-	target_compile_options(magic_get INTERFACE -DBOOST_PFR_USE_CPP17=0)
-endif()
-
 install(TARGETS pfr EXPORT magic_get_targets INCLUDES DESTINATION include)
 install(
     DIRECTORY ${CMAKE_SOURCE_DIR}/magic_get/include/
