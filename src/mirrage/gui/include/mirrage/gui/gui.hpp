34621d64754e1b10199842d39cad1f002f010df7
lowkey <mephistopheles@onlinehome.de>
2019-04-01 16:04:56 +0200

    fixed imgui <-> glm conversion functions

--- src/mirrage/gui/include/mirrage/gui/gui.hpp
+++ src/mirrage/gui/include/mirrage/gui/gui.hpp
@@ -15,22 +15,12 @@
 #include <glm/vec4.hpp>
 
 
-#define IM_VEC2_CLASS_EXTRA              \
-	constexpr ImVec2(const glm::vec2& f) \
-	{                                    \
-		x = f.x;                         \
-		y = f.y;                         \
-	}                                    \
-	constexpr operator glm::vec2() const { return glm::vec2(x, y); }
-#define IM_VEC4_CLASS_EXTRA              \
-	constexpr ImVec4(const glm::vec4& f) \
-	{                                    \
-		x = f.x;                         \
-		y = f.y;                         \
-		z = f.z;                         \
-		w = f.w;                         \
-	}                                    \
-	constexpr operator glm::vec4() const { return glm::vec4(x, y, z, w); }
+#define IM_VEC2_CLASS_EXTRA                        \
+	ImVec2(const glm::vec2& f) : x(f.x), y(f.y) {} \
+	operator glm::vec2() const { return glm::vec2(x, y); }
+#define IM_VEC4_CLASS_EXTRA                                        \
+	ImVec4(const glm::vec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {} \
+	operator glm::vec4() const { return glm::vec4(x, y, z, w); }
 
 #include <imgui.h>
 
